5e3303c2ed6dcbfc6d077521fe6ef5e8c6a33a91
Evgene <dzhek.web@yandex.ru>
2020-06-04 21:25:40 +0300

    add model & test verification commits

---
+++ SimpleCalendar/Widget/Model/SCDate.swift
@@ -0,0 +1,16 @@
+//
+//  SCDate.swift
+//  Widget
+//
+//  Created by Dzhek on 04.06.2020.
+//  Copyright © 2020 Dzhek. All rights reserved.
+//
+
+import Foundation
+
+struct SCDate {
+    let value: Int
+    let type: SCDateType
+    let isToday: Bool
+    
+}
---
+++ SimpleCalendar/Widget/Model/SCDateType.swift
@@ -0,0 +1,17 @@
+//
+//  SCDateType.swift
+//  Widget
+//
+//  Created by Dzhek on 04.06.2020.
+//  Copyright © 2020 Dzhek. All rights reserved.
+//
+
+import Foundation
+
+enum SCDateType {
+    case weekday
+    case weekend
+    case irrelevant
+    case weekNumber
+    
+}
